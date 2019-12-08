3f0af2628ac3f1ba7fd76c39b94bc49c8f866785
markoniemi <marko.j.niemi@gmail.com>
2019-12-08 20:39:38 +0200

    change /api/hello to POST.

--- server/src/main/java/org/example/controller/HelloController.java
+++ server/src/main/java/org/example/controller/HelloController.java
@@ -2,13 +2,14 @@ package org.example.controller;
 
 import java.util.Date;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @GetMapping("/api/hello")
-    public String hello() {
-        return "Hello, the time at the server is now " + new Date() + "\n";
+    @PostMapping("/api/hello")
+    public String hello(@RequestBody String name) {
+        return String.format("Hello %s, the time at the server is now %s\n", name, new Date());
     }
 }
