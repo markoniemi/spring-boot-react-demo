15962bbcbed9d4254ff1985f0403a6a470f7f8a9
markoniemi <marko.j.niemi@gmail.com>
2019-12-21 18:03:08 +0200

    Add typescript.

---
+++ frontend/src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import App from "./components/App";
+import ReactDOM = require("react-dom");
+
+ReactDOM.render(
+        <App />,
+    document.getElementById('root') as HTMLElement
+);
