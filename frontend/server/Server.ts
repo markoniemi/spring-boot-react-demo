f4298f149258667513e86edd978bdccbf425d97b
markoniemi <marko.j.niemi@gmail.com>
2019-12-25 20:43:30 +0200

    Get backend host and port from .env file.

--- frontend/server/Server.ts
+++ frontend/server/Server.ts
@@ -7,36 +7,35 @@ import * as logger from "winston";
 import webpackConfig from "../webpack.config";
 
 export default class Server {
-  private readonly compiler: Compiler;
-  private readonly devServerConfig: Configuration;
-  private server: Http.Server;
+    private readonly compiler: Compiler;
+    private readonly devServerConfig: Configuration;
+    private server: Http.Server;
 
-  public constructor(private readonly serverHost: string, private readonly serverPort: number,
-                     private readonly backendHost: string, private readonly backendPort: number) {
-    this.compiler = Webpack(webpackConfig);
-    this.devServerConfig = {
-      contentBase: "./public",
-      hot: true,
-      proxy: {
-//        "/api/*": `http://${backendHost}:${backendPort}`,
-        "/api/*": `http://localhost:8080`,
-      },
-      publicPath: "",
-    };
-  }
+    public constructor(private readonly serverHost: string, private readonly serverPort: number,
+                       private readonly backendHost: string, private readonly backendPort: number) {
+        this.compiler = Webpack(webpackConfig);
+        this.devServerConfig = {
+            contentBase: "./public",
+            hot: true,
+            proxy: {
+                "/api/*": `http://${backendHost}:${backendPort}`,
+            },
+            publicPath: "",
+        };
+    }
 
-  public start(): Http.Server {
-    const server = new WebpackDevServer(this.compiler, this.devServerConfig);
-    this.server = server.listen(this.serverPort, this.serverHost, (err) => {
-      if (err) {
-        logger.error(err.message);
-      }
-      logger.info(`Local web server runs at http://${this.serverHost}:${this.serverPort}`);
-    });
-    return this.server;
-  }
+    public start(): Http.Server {
+        const server = new WebpackDevServer(this.compiler, this.devServerConfig);
+        this.server = server.listen(this.serverPort, this.serverHost, (err) => {
+            if (err) {
+                logger.error(err.message);
+            }
+            logger.info(`Local web server runs at http://${this.serverHost}:${this.serverPort}`);
+        });
+        return this.server;
+    }
 
-  public async stop(): Promise<void> {
-    await this.server.close();
-  }
+    public async stop(): Promise<void> {
+        await this.server.close();
+    }
 }
