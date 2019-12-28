da13a3194da2604434c77156c6de4f4f43959d5d
Marko Niemi <marko.j.niemi@gmail.com>
2019-12-28 21:26:25 +0200

    UserContainer.

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -5,88 +5,14 @@ import Hello from "./Hello";
 import UserServiceMock from "../api/UserServiceMock";
 import User from "../domain/User";
 import {UserService} from "../api/UserService";
+import UserContainer from "./UserContainer";
 
-interface AppState {
-    users: User[];
-}
-
-class App extends Component<Readonly<{}>, AppState> {
-    private userService: UserService = new UserServiceMock();
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            users: [],
-        };
-        this.submitUser = this.submitUser.bind(this);
-        this.deleteUser = this.deleteUser.bind(this);
-        this.addUser = this.addUser.bind(this);
-    }
-
-    public async componentWillMount() {
-        await this.fetchUsers();
-    }
-
-    public addUser() {
-        let updatedUsers = [...this.state.users, new User("", "", "")];
-        this.setState((prevState, props) => ({users: updatedUsers}));
-    }
-
-    public async deleteUser(id: number) {
-        if (window.confirm("Do you want to delete this item") === true) {
-            await this.userService.delete(id);
-            this.fetchUsers();
-        }
-    }
-
-    public async submitUser(user: User) {
-        if (user.id == undefined) {
-            await this.userService.create(user);
-        } else {
-            await this.userService.update(user);
-        }
-        this.fetchUsers();
-    }
-
-    private async fetchUsers() {
-        let users: User[] = await this.userService.fetchUsers();
-        this.setState((prevState, props) => ({users: users}));
-    }
-
+class App extends Component<Readonly<{}>, Readonly<{}>> {
     public render() {
         return (
             <div>
                 <div className="container-fluid">
-                    <div className="row mt-3">
-                        <div className="col-lg-12">
-                            <div className="card">
-                                <div className="card-header">
-                                    Users
-                                </div>
-                                <div className="card-body">
-                                    <table className="table table-hover">
-                                        <thead className="thead-dark">
-                                        <tr>
-                                            <th>Username</th>
-                                            <th>Password</th>
-                                            <th>Email</th>
-                                            <th>Edit/Save</th>
-                                            <th>Delete</th>
-                                        </tr>
-                                        </thead>
-                                        <UserList
-                                            deleteUser={this.deleteUser}
-                                            users={this.state.users}
-                                            submitUser={this.submitUser}
-                                        />
-                                    </table>
-                                    <button id="addUser" className="btn btn-dark pull-left" onClick={this.addUser}>
-                                        Add New
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <UserContainer/>
                 </div>
                 <Hello/>
             </div>
---
+++ frontend/src/components/UserContainer.tsx
@@ -0,0 +1,92 @@
+import React, {Component} from "react";
+import "../App.css";
+import UserList from "./UserList";
+import Hello from "./Hello";
+import UserServiceMock from "../api/UserServiceMock";
+import User from "../domain/User";
+import {UserService} from "../api/UserService";
+
+interface UserContainerState {
+    users: User[];
+}
+
+class UserContainer extends Component<Readonly<{}>, UserContainerState> {
+    private userService: UserService = new UserServiceMock();
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            users: [],
+        };
+        this.submitUser = this.submitUser.bind(this);
+        this.deleteUser = this.deleteUser.bind(this);
+        this.addUser = this.addUser.bind(this);
+    }
+
+    public async componentWillMount() {
+        await this.fetchUsers();
+    }
+
+    public addUser() {
+        let updatedUsers = [...this.state.users, new User("", "", "")];
+        this.setState((prevState, props) => ({users: updatedUsers}));
+    }
+
+    public async deleteUser(id: number) {
+        if (window.confirm("Do you want to delete this item") === true) {
+            await this.userService.delete(id);
+            this.fetchUsers();
+        }
+    }
+
+    public async submitUser(user: User) {
+        if (user.id == undefined) {
+            await this.userService.create(user);
+        } else {
+            await this.userService.update(user);
+        }
+        this.fetchUsers();
+    }
+
+    private async fetchUsers() {
+        let users: User[] = await this.userService.fetchUsers();
+        this.setState((prevState, props) => ({users: users}));
+    }
+
+    public render() {
+        return (
+            <div className="row mt-3">
+                <div className="col-lg-12">
+                    <div className="card">
+                        <div className="card-header">
+                            Users
+                        </div>
+                        <div className="card-body">
+                            <table className="table table-hover">
+                                <thead className="thead-dark">
+                                <tr>
+                                    <th>Username</th>
+                                    <th>Password</th>
+                                    <th>Email</th>
+                                    <th>Edit/Save</th>
+                                    <th>Delete</th>
+                                </tr>
+                                </thead>
+                                <UserList
+                                    deleteUser={this.deleteUser}
+                                    users={this.state.users}
+                                    submitUser={this.submitUser}
+                                />
+                            </table>
+                            <button id="addUser" className="btn btn-dark pull-left" onClick={this.addUser}>
+                                Add New
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default UserContainer;
