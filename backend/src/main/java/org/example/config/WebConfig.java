13a834b2c02c2c073d714b88500690e1fd44589a
markoniemi <marko.j.niemi@gmail.com>
2020-01-04 10:30:08 +0200

    Fix index.html and WebConfig to support bookmarking with react-router. Skip maven-frontend-plugin goals in Eclipse. Remove some obsolete dependencies in package.json.

--- backend/src/main/java/org/example/config/WebConfig.java
+++ backend/src/main/java/org/example/config/WebConfig.java
@@ -11,9 +11,12 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/frontend/0.0.1-SNAPSHOT/");
     }
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/")
-                .setViewName("forward:/index.html");
+        registry.addViewController("/").setViewName("forward:/index.html");
+        registry.addViewController("/{spring:\\w+}").setViewName("forward:/index.html");
+        registry.addViewController("/**/{spring:\\w+}").setViewName("forward:/index.html");
+        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}").setViewName("forward:/index.html");
     }
 }
