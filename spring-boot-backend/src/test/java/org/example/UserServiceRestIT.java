4785966cc12a3413879f4eed0724aab043543b08
markoniemi <marko.j.niemi@gmail.com>
2020-02-19 17:20:29 +0200

    RestIT improvement.

--- spring-boot-backend/src/test/java/org/example/UserServiceRestIT.java
+++ spring-boot-backend/src/test/java/org/example/UserServiceRestIT.java
@@ -3,6 +3,9 @@ package org.example;
 import static org.junit.Assert.*;
 
 import org.example.config.IntegrationTestConfig;
+import org.example.model.user.Role;
+import org.example.model.user.User;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,6 +13,9 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.ContextHierarchy;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
@@ -22,9 +28,26 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 public class UserServiceRestIT {
     private String url = "http://localhost:8080";
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    public void create() throws JsonProcessingException {
+        String userJson = objectMapper.writeValueAsString(new User("username", "password", "email", Role.ROLE_USER));
+        User user = RestAssured.given().body(userJson).header("Content-Type", "application/json")
+                .header("Accept", "application/json").post(url + "/api/rest/users").then().statusCode(200).extract()
+                .as(User.class);
+        Assert.assertNotNull(user);
+        Assert.assertNotNull(user.getId());
+        user = RestAssured.get(url + "/api/rest/users/"+user.getId()).then().statusCode(200).extract().as(User.class);
+        Assert.assertNotNull(user);
+        Assert.assertNotNull(user.getId());
+        RestAssured.when().delete(url + "/api/rest/users/"+user.getId()).then()
+        .statusCode(204);
+    }
 
     @Test
     public void deleteNonExistent() {
-        ExtractableResponse<Response> response = RestAssured.when().delete(url + "/api/rest/users/10000").then().statusCode(404).extract();
+        ExtractableResponse<Response> response = RestAssured.when().delete(url + "/api/rest/users/10000").then()
+                .statusCode(404).extract();
     }
 }
