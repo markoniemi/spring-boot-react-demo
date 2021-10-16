db819b74ce819c46e841682c0cecd03201e0ad7f
Marko Niemi <marko.j.niemi@gmail.com>
2021-10-16 14:10:58 +0300

    Jwt security, first working implementation.

--- spring-boot-backend/src/test/java/org/example/config/IntegrationTestConfig.java
+++ spring-boot-backend/src/test/java/org/example/config/IntegrationTestConfig.java
@@ -1,10 +1,22 @@
 package org.example.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+
+import feign.RequestInterceptor;
 
 @Configuration
 @Import({ SeleniumConfig.class })
 public class IntegrationTestConfig {
 
+    @Bean
+    public RequestInterceptor jwtRequestInterceptor() {
+        return new JwtRequestInterceptor();
+    }
+    @Bean
+    public ClientHttpRequestInterceptor restRequestInterceptor() {
+        return new RestRequestInterceptor();
+    }
 }
