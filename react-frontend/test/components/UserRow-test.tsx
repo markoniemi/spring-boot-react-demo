d3c414958a0ccc131937e7213445cec29a14a5e1
Marko Niemi <marko.j.niemi@gmail.com>
2021-11-15 19:02:39 +0200

    Test improvements.

--- react-frontend/test/components/UserRow-test.tsx
+++ react-frontend/test/components/UserRow-test.tsx
@@ -4,12 +4,13 @@ import * as React from "react";
 import UserRow from "../../src/components/UserRow";
 import User from "../../src/domain/User";
 import { user1 } from "../userList";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, RouteComponentProps } from "react-router-dom";
 import createRouteComponentProps from "../RouteComponentPropsMock";
 import { act, configure, fireEvent, render, screen } from "@testing-library/react";
 import i18nConfig from "../../src/messages/messages";
 import { IntlProvider } from "react-intl";
 import { findButton } from "./EditUser-test";
+import { Table } from "react-bootstrap";
 
 describe("UserRow component", () => {
     beforeEach(() => {
@@ -18,42 +19,41 @@ describe("UserRow component", () => {
     });
     test("should render a user", async () => {
         const routeComponentProps = createRouteComponentProps(null);
-        render(
-            <IntlProvider locale={i18nConfig.locale} messages={i18nConfig.messages}>
-                <BrowserRouter>
-                    <UserRow.WrappedComponent user={user1} deleteUser={null} {...routeComponentProps} />
-                </BrowserRouter>
-            </IntlProvider>,
-        );
+        renderUserRow(user1, null, routeComponentProps);
         assert.equal((await screen.findByTestId("username")).textContent, "user1");
         assert.equal((await screen.findByTestId("email")).textContent, "email1");
     });
     test("should not create error with empty user", async () => {
         const emptyUser = new User();
         const routeComponentProps = createRouteComponentProps(null);
-        render(
-            <IntlProvider locale={i18nConfig.locale} messages={i18nConfig.messages}>
-                <BrowserRouter>
-                    <UserRow.WrappedComponent user={emptyUser} deleteUser={null} {...routeComponentProps} />
-                </BrowserRouter>
-            </IntlProvider>,
-        );
+        renderUserRow(emptyUser, null, routeComponentProps);
         assert.equal((await screen.findByTestId("username")).textContent, "");
         assert.equal((await screen.findByTestId("email")).textContent, "");
     });
     test("should delete a user", async () => {
         const deleteUser = jest.fn();
         const routeComponentProps = createRouteComponentProps(null);
-        render(
-            <IntlProvider locale={i18nConfig.locale} messages={i18nConfig.messages}>
-                <BrowserRouter>
-                    <UserRow.WrappedComponent user={user1} deleteUser={deleteUser} {...routeComponentProps} />,
-                </BrowserRouter>
-            </IntlProvider>,
-        );
+        renderUserRow(user1, deleteUser, routeComponentProps);
         await act(async () => {
             await fireEvent.click(await findButton("delete"));
         });
         expect(deleteUser).toBeCalledWith(1);
     });
 });
+function renderUserRow(
+    user: User,
+    deleteUser: (id: number) => void,
+    routeComponentProps: RouteComponentProps<any>,
+): void {
+    render(
+        <Table>
+            <tbody>
+                <IntlProvider locale={i18nConfig.locale} messages={i18nConfig.messages}>
+                    <BrowserRouter>
+                        <UserRow.WrappedComponent user={user} deleteUser={deleteUser} {...routeComponentProps} />
+                    </BrowserRouter>
+                </IntlProvider>
+            </tbody>
+        </Table>,
+    );
+}
