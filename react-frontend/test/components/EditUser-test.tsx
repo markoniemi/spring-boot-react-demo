559f5a220cb8fff5b73fdb5ecfa21591a30cade2
Marko Niemi <marko.j.niemi@gmail.com>
2021-11-20 09:59:11 +0200

    Test improvements.

--- react-frontend/test/components/EditUser-test.tsx
+++ react-frontend/test/components/EditUser-test.tsx
@@ -27,6 +27,19 @@ export async function selectOption(id: string, value: string): Promise<void> {
     fireEvent.change((await screen.getByTestId(id)) as HTMLInputElement, { target: { value: value } });
 }
 
+async function assertUser(id: string, username: string, email: string, role: string): Promise<void> {
+    assert.equal(await getValueById("id"), id);
+    assert.equal(await getValueById("username"), username);
+    assert.equal(await getValueById("email"), email);
+    assert.equal(await getValueById("role"), role);
+}
+async function setUser(username: string, password: string, email: string, role: string): Promise<void> {
+    await setText("username", username);
+    await setText("password", password);
+    await setText("email", email);
+    await selectOption("role", role);
+}
+
 describe("EditUser component", () => {
     beforeEach(() => {
         configure({ testIdAttribute: "id" });
@@ -37,32 +50,21 @@ describe("EditUser component", () => {
     });
     test("should render a user", async () => {
         fetchMock.getOnce("/api/rest/users/1", user1);
-        const routeComponentProps = createRouteComponentProps({ id: "1" });
-        renderEditUser(routeComponentProps);
+        renderEditUser(createRouteComponentProps({ id: "1" }));
         await sleep(100);
-        assert.equal(await getValueById("id"), "1");
-        assert.equal(await getValueById("username"), "user1");
-        assert.equal(await getValueById("email"), "email1");
+        await assertUser(user1.id.toString(), user1.username, user1.email, user1.role.toString());
     });
     test("should show an error", async () => {
         fetchMock.getOnce("/api/rest/users/1", 401);
-        const routeComponentProps = createRouteComponentProps({ id: "1" });
-        renderEditUser(routeComponentProps);
+        renderEditUser(createRouteComponentProps({ id: "1" }));
         await sleep(100);
         assert.isNotNull(await screen.getByText("Error loading user"));
     });
     test.todo("should add a user");
     test("should show validation error with empty user", async () => {
-        const routeComponentProps = createRouteComponentProps({});
-        render(
-            <IntlProvider locale={i18nConfig.locale} messages={i18nConfig.messages}>
-                <EditUser.WrappedComponent {...routeComponentProps} />
-            </IntlProvider>,
-        );
+        renderEditUser(createRouteComponentProps({}));
         await sleep(100);
-        assert.equal(await getValueById("id"), "");
-        assert.equal(await getValueById("username"), "");
-        assert.equal(await getValueById("email"), "");
+        await assertUser("", "", "", "");
         await act(async () => {
             fireEvent.click(await findButton("saveUser"));
             await sleep(100);
@@ -74,19 +76,11 @@ describe("EditUser component", () => {
         // assert.isNotNull(await screen.getByText("Role required"));
     });
     test("should show an error with invalid user", async () => {
-        const routeComponentProps = createRouteComponentProps({});
-        render(
-            <IntlProvider locale={i18nConfig.locale} messages={i18nConfig.messages}>
-                <EditUser.WrappedComponent {...routeComponentProps} />
-            </IntlProvider>,
-        );
+        renderEditUser(createRouteComponentProps({}));
         await sleep(100);
         fetchMock.postOnce("/api/rest/users/", 404);
         await act(async () => {
-            await setText("username", "invalid");
-            await setText("password", "invalid");
-            await setText("email", "invalid");
-            await selectOption("role", "ROLE_ADMIN");
+            await setUser("invalid", "invalid", "invalid", "ROLE_ADMIN");
             fireEvent.click(await findButton("saveUser"));
             await sleep(100);
         });
@@ -98,13 +92,10 @@ describe("EditUser component", () => {
         routeComponentProps.history.push = jest.fn();
         renderEditUser(routeComponentProps);
         await sleep(100);
-        assert.equal(await getValueById("username"), "user1");
-        assert.equal(await getValueById("email"), "email1");
+        await assertUser(user1.id.toString(), user1.username, user1.email, user1.role.toString());
         await act(async () => {
-            await setText("username", "newUsername");
-            await setText("email", "newEmail");
-            assert.equal(await getValueById("username"), "newUsername");
-            assert.equal(await getValueById("email"), "newEmail");
+            setUser("newUsername", "newPassword", "newEmail", "ROLE_USER");
+            assertUser("1", "newUsername", "newEmail", "ROLE_USER");
             fetchMock.putOnce("/api/rest/users/1", { username: "newUsername", email: "newEmail" });
             fireEvent.keyPress(await screen.findByTestId("email"), { key: "Enter", code: "Enter", charCode: 13 });
             await sleep(100);
