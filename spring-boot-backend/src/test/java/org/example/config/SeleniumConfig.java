84fbd5956c907e983f1dc8c981a805ae131eee7f
Marko Niemi <marko.j.niemi@gmail.com>
2022-12-25 08:21:45 +0200

    webdrivermanager -> 5.1.1

--- spring-boot-backend/src/test/java/org/example/config/SeleniumConfig.java
+++ spring-boot-backend/src/test/java/org/example/config/SeleniumConfig.java
@@ -8,13 +8,13 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
-import io.github.bonigarcia.wdm.ChromeDriverManager;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 @ComponentScan(basePackages = "org.selenium")
 public class SeleniumConfig {
     @Bean
     public WebDriver getWebDriver() {
-        ChromeDriverManager.chromedriver().setup();
+        WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("headless");
         options.addArguments("window-size=1200x800");
