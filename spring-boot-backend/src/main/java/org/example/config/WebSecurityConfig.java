f02cc994912feae85c2e0e770a3bb96a6de971a2
Marko Niemi <marko.j.niemi@gmail.com>
2023-04-10 11:43:16 +0300

    use web.ignoring instead of http.permitAll.

--- spring-boot-backend/src/main/java/org/example/config/WebSecurityConfig.java
+++ spring-boot-backend/src/main/java/org/example/config/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -29,10 +30,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   String[] ignoredPaths = {"/*", "/login", "/api/rest/auth/login/**", "/h2-console/**"};
 
   @Override
+  public void configure(WebSecurity web) throws Exception {
+    web.ignoring().antMatchers(ignoredPaths);
+  }
+  @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.cors().and().csrf().disable();
     http.authorizeRequests().regexMatchers(".*\\?wsdl").permitAll()//
-        .antMatchers(ignoredPaths).permitAll()//
+//        .antMatchers(ignoredPaths).permitAll()//
         .anyRequest().authenticated()//
         .and()//
         .addFilter(new JwtAuthenticationFilter(authenticationManager()))//
