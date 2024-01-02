9e436328fe3f6753a819945a69d1d26ca62d1df3
Marko Niemi <marko.j.niemi@gmail.com>
2024-01-02 19:59:53 +0200

    Remove RestServiceFactory and clean up RestConfig.

--- spring-boot-backend/src/main/java/org/example/config/RestConfig.java
+++ spring-boot-backend/src/main/java/org/example/config/RestConfig.java
@@ -1,18 +1,18 @@
 package org.example.config;
 
-import jakarta.annotation.Resource;
-
-import org.apache.cxf.bus.spring.SpringBus;
+import org.apache.cxf.Bus;
 import org.apache.cxf.endpoint.Server;
 import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
-import org.apache.cxf.transport.servlet.CXFServlet;
+import org.example.rest.BindExceptionMapper;
+import org.example.rest.ConstraintViolationExceptionMapper;
+import org.example.rest.EntityNotFoundExceptionMapper;
 import org.example.service.user.LoginService;
 import org.example.service.user.TimeService;
 import org.example.service.user.UserService;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
+import com.fasterxml.jackson.jakarta.rs.json.JacksonXmlBindJsonProvider;
+import jakarta.annotation.Resource;
 
 @Configuration
 public class RestConfig {
@@ -22,26 +22,19 @@ public class RestConfig {
     private TimeService helloService;
     @Resource
     private LoginService loginService;
-
-    @Bean(destroyMethod = "shutdown")
-    public SpringBus cxf() {
-        return new SpringBus();
-    }
+    @Resource
+    private Bus bus;
 
     @Bean(destroyMethod = "destroy")
-    @DependsOn("cxf")
     public Server jaxRsServer() {
-        JAXRSServerFactoryBean factory = new RestServiceFactory();
-        factory.setBus(cxf());
+        JAXRSServerFactoryBean factory = new JAXRSServerFactoryBean();
+        factory.setProvider(new JacksonXmlBindJsonProvider());
+        factory.setProvider(new BindExceptionMapper());
+        factory.setProvider(new EntityNotFoundExceptionMapper());
+        factory.setProvider(new ConstraintViolationExceptionMapper());
+        factory.setBus(bus);
         factory.setAddress("/rest");
         factory.setServiceBeanObjects(userService, helloService, loginService);
         return factory.create();
-    }
-
-    @Bean
-    public ServletRegistrationBean cxfServlet() {
-        final ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new CXFServlet(), "/api/*");
-        servletRegistrationBean.setLoadOnStartup(1);
-        return servletRegistrationBean;
     }
 }
