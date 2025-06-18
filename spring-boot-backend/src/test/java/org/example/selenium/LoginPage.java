6d82869c82ba761f2311e088f048972ec6af9523
Marko Niemi <marko.j.niemi@gmail.com>
2025-06-18 16:52:31 +0300

    Try fixing build error in Azure pipelines.

--- spring-boot-backend/src/test/java/org/example/selenium/LoginPage.java
+++ spring-boot-backend/src/test/java/org/example/selenium/LoginPage.java
@@ -2,15 +2,13 @@ package org.example.selenium;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import lombok.extern.slf4j.Slf4j;
-@Slf4j
+
 public class LoginPage extends AbstractPage {
     public LoginPage(WebDriver webDriver) {
         super(webDriver);
     }
 
     public void login(String username, String password) {
-        log.debug(webDriver.getPageSource());
         setText(By.id("username"), username);
         setText(By.id("password"), password);
         click(By.id("login"));
