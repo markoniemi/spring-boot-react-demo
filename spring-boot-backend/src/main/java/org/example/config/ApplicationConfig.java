bf75444dc43f22561fc8ff0c05d8cb2d8e684b55
Marko Niemi <marko.j.niemi@gmail.com>
2025-07-03 09:19:58 +0300

    Log4j2 colors and clean-up.

--- spring-boot-backend/src/main/java/org/example/config/ApplicationConfig.java
+++ spring-boot-backend/src/main/java/org/example/config/ApplicationConfig.java
@@ -1,12 +1,10 @@
 package org.example.config;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
 @ComponentScan(basePackages = "org.example")
-@EntityScan(basePackages = "org.example.model")
 @Import({JpaConfig.class, WebServiceConfig.class, RestConfig.class})
 public class ApplicationConfig {}
--- spring-boot-backend/src/main/java/org/example/config/WebConfig.java
+++ spring-boot-backend/src/main/java/org/example/config/WebConfig.java
@@ -8,35 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    registry
-        .addResourceHandler("/**")
-        .addResourceLocations(
-            "classpath:/META-INF/resources/webjars/react-frontend/0.0.1-SNAPSHOT/");
+    registry.addResourceHandler("/**").addResourceLocations(
+        "classpath:/META-INF/resources/webjars/react-frontend/0.0.1-SNAPSHOT/");
   }
-
-  // @Override
-  // public void addViewControllers(ViewControllerRegistry registry) {
-  // registry.addViewController("/").setViewName("forward:/index.html");
-  // registry.addViewController("/{path:[^\\.]*}").setViewName("forward:/index.html");
-  // registry.addViewController("/**/{path:[^\\.]*}").setViewName("forward:/index.html");
-  // registry.addViewController("/**/{path:[^\\.]*}/**").setViewName("forward:/index.html");
-  // }
-
-  // @Override
-  // public void addViewControllers(ViewControllerRegistry registry) {
-  // registry.addViewController("/").setViewName("forward:/index.html");
-  // registry.addViewController("/{spring:\\w+}").setViewName("forward:/index.html");
-  // }
-
-  // @Override
-  // public void addViewControllers(ViewControllerRegistry registry) {
-  // // registry.addViewController("/{x:^(?!api$).*$}")
-  // // .setViewName("forward:/index.html");
-  // // registry.addViewController("/**/{spring:\\w+}")
-  // // .setViewName("forward:/");
-  // // registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}")
-  // // .setViewName("forward:/");
-  // }
 
   @Override
   public void addViewControllers(ViewControllerRegistry registry) {
@@ -44,14 +18,4 @@ public class WebConfig implements WebMvcConfigurer {
     registry.addViewController("/login").setViewName("forward:/index.html");
     registry.addViewController("/users/**").setViewName("forward:/index.html");
   }
-
-  // @Override
-  // public void addViewControllers(ViewControllerRegistry registry) {
-  // direct all paths except /api to index.html
-  // this does not work in spring 2.6 unless spring.mvc.pathmatch.matching-strategy=ant_path_matcher
-  // is set in application.properties
-  // registry.addViewController("/{x:[\\w\\-]+}").setViewName("forward:/index.html");
-  // registry.addViewController("/{x:^(?!api$).*$}/**/{y:[\\w\\-]+}/**")
-  // .setViewName("forward:/index.html");
-  // }
 }
