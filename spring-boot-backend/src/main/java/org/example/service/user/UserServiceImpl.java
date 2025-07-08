740893e7482025ebc00355cf6e0191fed22df5fa
Marko Niemi <marko.j.niemi@gmail.com>
2025-07-08 20:41:29 +0300

    Revert "Try io.github.markoniemi:interface-log from github packagies."
    This reverts commit c6816d4f51861a02dc9d727bca70aabde013b57f.

--- spring-boot-backend/src/main/java/org/example/service/user/UserServiceImpl.java
+++ spring-boot-backend/src/main/java/org/example/service/user/UserServiceImpl.java
@@ -3,7 +3,6 @@ package org.example.service.user;
 import java.util.List;
 import org.apache.commons.collections4.IterableUtils;
 import org.apache.commons.lang3.Validate;
-import org.example.log.InterfaceLog;
 import org.example.model.user.User;
 import org.example.repository.user.UserRepository;
 import org.springframework.context.annotation.Primary;
@@ -18,13 +17,11 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 @Service(value = "userService")
 @WebService(endpointInterface = "org.example.service.user.UserService", serviceName = "UserService")
-@InterfaceLog
 public class UserServiceImpl implements UserService {
   @Resource UserRepository userRepository;
 
   @Override
   @Transactional
-  @InterfaceLog
   public List<User> findAll() {
     log.trace("findAll");
     return IterableUtils.toList(userRepository.findAll());
@@ -32,7 +29,6 @@ public class UserServiceImpl implements UserService {
 
   @Override
   @Transactional
-  @InterfaceLog
   public List<User> search(UserSearchForm searchForm) {
     log.trace("search: {}", searchForm);
     // findByUsernameOrEmailOrRole returns nothing if searchForm is empty
@@ -45,7 +41,6 @@ public class UserServiceImpl implements UserService {
 
   @Override
   @Transactional
-  @InterfaceLog
   public User create(User user) throws ConstraintViolationException {
     Validate.notNull(user, "invalid.user");
     Validate.isTrue(!userRepository.existsByUsername(user.getUsername()), "existing.username");
@@ -55,7 +50,6 @@ public class UserServiceImpl implements UserService {
 
   @Override
   @Transactional
-  @InterfaceLog
   public User update(User user) throws ConstraintViolationException {
     Validate.notNull(user, "invalid.user");
     Validate.isTrue(userRepository.existsById(user.getId()), "nonexistent.user");
@@ -65,7 +59,6 @@ public class UserServiceImpl implements UserService {
 
   @Override
   @Transactional
-  @InterfaceLog
   public User findById(Long id) {
     log.trace("findById: {}", id);
     Validate.notNull(id, "null.id");
@@ -74,7 +67,6 @@ public class UserServiceImpl implements UserService {
 
   @Override
   @Transactional
-  @InterfaceLog
   public User findByUsername(String username) {
     log.trace("findByUsername: {}", username);
     return userRepository.findByUsername(username);
@@ -82,7 +74,6 @@ public class UserServiceImpl implements UserService {
 
   @Override
   @Transactional
-  @InterfaceLog
   public boolean exists(Long id) {
     log.trace("exists: {}", id);
     return userRepository.findById(id) != null;
@@ -90,7 +81,6 @@ public class UserServiceImpl implements UserService {
 
   @Override
   @Transactional
-  @InterfaceLog
   /** If the entity is not found in the persistence store it is silently ignored. */
   public void delete(Long id) {
     log.trace("delete: {}", id);
@@ -99,7 +89,6 @@ public class UserServiceImpl implements UserService {
 
   @Override
   @Transactional
-  @InterfaceLog
   public long count() {
     return userRepository.count();
   }
