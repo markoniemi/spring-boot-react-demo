c58449e910a516c8a83d62d9cd8101cf32a9cff7
Marko Niemi <marko.j.niemi@gmail.com>
2025-07-21 10:46:51 +0300

    CustomPropertySource demo, uses property from application.properties.

--- spring-boot-backend/src/main/java/org/example/config/ContextInitializer.java
+++ spring-boot-backend/src/main/java/org/example/config/ContextInitializer.java
@@ -3,18 +3,12 @@ package org.example.config;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.env.MutablePropertySources;
-import org.springframework.core.env.PropertySource;
-import lombok.extern.log4j.Log4j2;
-@Log4j2
+
 public class ContextInitializer implements ApplicationContextInitializer {
   @Override
   public void initialize(final ConfigurableApplicationContext context) {
-    log.debug("ContextInitializer.initialize");
     ConfigurableEnvironment environment = context.getEnvironment();
-    MutablePropertySources propertySources = environment.getPropertySources();
-    PropertySource propertySource = new CustomPropertiesPropertySource("config");
-    propertySources.addLast(propertySource);
-//    propertySources.addFirst(propertySource);
+    String url = environment.getProperty("config.url", "");
+    environment.getPropertySources().addLast(new CustomPropertySource("config", url));
   }
 }
--- spring-boot-backend/src/main/java/org/example/config/CustomPropertySource.java
+++ spring-boot-backend/src/main/java/org/example/config/CustomPropertySource.java
@@ -2,11 +2,12 @@ package org.example.config;
 
 import org.springframework.core.env.PropertySource;
 import lombok.extern.log4j.Log4j2;
+
 @Log4j2
-public class CustomPropertiesPropertySource extends PropertySource {
-  public CustomPropertiesPropertySource(String name) {
+public class CustomPropertySource extends PropertySource {
+  public CustomPropertySource(String name, String url) {
     super(name);
-    log.debug("CustomPropertiesPropertySource: {}", name);
+    log.debug("init {} with url: {}", name, url);
   }
 
   @Override
