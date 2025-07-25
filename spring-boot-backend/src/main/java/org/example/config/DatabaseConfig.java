42a2f6b8aa250a3a5eece40d21abe1c4cd3aa4e4
Marko Niemi <marko.j.niemi@gmail.com>
2025-07-25 10:00:25 +0300

    Add support for hsqldb. H2 is default. Use mvn install -P hsqldb, or -Dspring.profiles.active=hsqldb.

--- spring-boot-backend/src/main/java/org/example/config/DatabaseConfig.java
+++ spring-boot-backend/src/main/java/org/example/config/DatabaseConfig.java
@@ -4,7 +4,9 @@ import java.sql.SQLException;
 import org.h2.tools.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 @Configuration
+@Profile("h2")
 public class DatabaseConfig {
   @Bean(initMethod = "start", destroyMethod = "stop")
   public Server h2Server() throws SQLException {
